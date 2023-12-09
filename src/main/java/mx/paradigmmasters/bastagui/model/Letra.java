91e098cda2f71a4c9d061a087f02df6623512c81
Fernanda G M <fergarci0103@gmail.com>
2023-12-09 16:11:20 -0600

    Documentación en Javadoc de la clase Letra

--- src/main/java/mx/paradigmmasters/bastagui/model/Letra.java
+++ src/main/java/mx/paradigmmasters/bastagui/model/Letra.java
@@ -1,8 +1,22 @@
 package mx.paradigmmasters.bastagui.model;
 
+/**
+ * Clase que representa una letra utilizada en el juego.
+ * Esta clase almacena una letra y proporciona un método para recuperarla.
+ *
+ * @author García Martínez María Fernanda
+ * @author Hernández Meza Víctor Yahir
+ * @author Márquez López María Fernanda
+ * @author Martínez Florez Uri
+ */
 public class Letra {
     private String letra;
 
+    /**
+     * Obtiene la letra almacenada en esta instancia.
+     *
+     * @return La letra almacenada.
+     */
     public String getLetra() {
         return letra;
     }
