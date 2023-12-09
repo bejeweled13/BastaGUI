1a32cc1a53d2144e9d4f305c648112b81e884fd7
Fernanda G M <fergarci0103@gmail.com>
2023-12-09 16:07:48 -0600

    Documentación en Javadoc de la clase Constants

--- src/main/java/mx/paradigmmasters/bastagui/model/Constants.java
+++ src/main/java/mx/paradigmmasters/bastagui/model/Constants.java
@@ -1,7 +1,28 @@
 package mx.paradigmmasters.bastagui.model;
 
+/**
+ * Clase que contiene constantes utilizadas a lo largo del juego Bastagui.
+ * Define varios estados del juego para su manejo en diferentes partes del código.
+ *
+ * @author García Martínez María Fernanda
+ * @author Hernández Meza Víctor Yahir
+ * @author Márquez López María Fernanda
+ * @author Martínez Florez Uri
+ */
 public class Constants {
+
+    /**
+     * Constante que representa el estado 'Cargando' del juego.
+     */
     public static final int CARGANDO = 1;
+
+    /**
+     * Constante que representa el estado 'Listo' del juego.
+     */
     public static final int LISTO = 2;
+
+    /**
+     * Constante que representa el estado 'Finalizado' del juego.
+     */
     public static final int FINALIZADO = 3;
 }
