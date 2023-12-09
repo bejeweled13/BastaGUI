b0db3a031ae98c03b34eaf4417bb6f10ccd6de6c
Fernanda G M <fergarci0103@gmail.com>
2023-12-09 16:17:55 -0600

    Documentación en Javadoc de la clase Main

--- src/main/java/mx/paradigmmasters/bastagui/Main.java
+++ src/main/java/mx/paradigmmasters/bastagui/Main.java
@@ -3,7 +3,22 @@ package mx.paradigmmasters.bastagui;
 import mx.paradigmmasters.bastagui.control.MainControl;
 import mx.paradigmmasters.bastagui.view.JoinView;
 
+/**
+ * Clase principal de la aplicación del juego Basta.
+ * Esta clase contiene el método main, que es el punto de entrada de la aplicación.
+ *
+ * @author García Martínez María Fernanda
+ * @author Hernández Meza Víctor Yahir
+ * @author Márquez López María Fernanda
+ * @author Martínez Florez Uri
+ */
 public class Main {
+
+    /**
+     * Método principal que inicia la aplicación.
+     *
+     * @param args Argumentos de línea de comandos.
+     */
     public static void main(String[] args) {
         new MainControl();
     }
