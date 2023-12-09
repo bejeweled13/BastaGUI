8f62da4df71aa2375e3010f5226d06132d47cf26
Fernanda G M <fergarci0103@gmail.com>
2023-12-09 16:08:41 -0600

    Documentación en Javadoc de la clase Estado

--- src/main/java/mx/paradigmmasters/bastagui/model/Estado.java
+++ src/main/java/mx/paradigmmasters/bastagui/model/Estado.java
@@ -1,9 +1,23 @@
 package mx.paradigmmasters.bastagui.model;
 
+/**
+ * Clase que representa el estado actual del juego y la puntuación del jugador.
+ * Mantiene información sobre el estado del juego utilizando constantes definidas en {@link Constants}.
+ *
+ * @author García Martínez María Fernanda
+ * @author Hernández Meza Víctor Yahir
+ * @author Márquez López María Fernanda
+ * @author Martínez Florez Uri
+ */
 public class Estado {
     private int estado = Constants.CARGANDO;
     private int puntos = 0;
 
+    /**
+     * Obtiene el estado actual del juego.
+     *
+     * @return El estado del juego, utilizando las constantes definidas en {@link Constants}.
+     */
     public int getEstado() {
         return estado;
     }
@@ -12,6 +26,11 @@ public class Estado {
         this.estado = estado;
     }
 
+    /**
+     * Obtiene la puntuación actual del jugador.
+     *
+     * @return La puntuación actual del jugador.
+     */
     public int getPuntos() {
         return puntos;
     }
