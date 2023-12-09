7b8d43e26e9d02e586530c1d48a0dc1a4649f87b
Fernanda G M <fergarci0103@gmail.com>
2023-12-09 16:13:18 -0600

    Documentación en Javadoc de la clase ListenerThread

--- src/main/java/mx/paradigmmasters/bastagui/threads/ListenerThread.java
+++ src/main/java/mx/paradigmmasters/bastagui/threads/ListenerThread.java
@@ -9,15 +9,34 @@ import mx.paradigmmasters.bastagui.model.JsonSerializer;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
+/**
+ * Hilo que escucha los mensajes entrantes y actualiza el control principal del juego basado en estos mensajes.
+ * Deserializa los mensajes JSON entrantes y realiza acciones en la interfaz de usuario y lógica del juego.
+ *
+ * @author García Martínez María Fernanda
+ * @author Hernández Meza Víctor Yahir
+ * @author Márquez López María Fernanda
+ * @author Martínez Florez Uri
+ */
 public class ListenerThread extends Thread{
     private final ObjectInputStream input;
     private final MainControl mainControl;
 
+    /**
+     * Constructor que inicializa el hilo con un flujo de entrada y control principal.
+     *
+     * @param input El flujo de entrada para escuchar los mensajes.
+     * @param mainControl El control principal del juego para actualizar la interfaz y la lógica.
+     */
     public ListenerThread(ObjectInputStream input, MainControl mainControl) {
         this.input = input;
         this.mainControl = mainControl;
     }
 
+    /**
+     * Método que se ejecuta cuando el hilo inicia.
+     * Escucha y procesa los mensajes entrantes, actualizando el estado del juego y la interfaz de usuario.
+     */
     @Override
     public void run() {
         super.run();
