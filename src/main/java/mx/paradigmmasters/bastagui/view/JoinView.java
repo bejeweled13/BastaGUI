9530c3224db2a52312b10ff2deba7473179a83d0
Fernanda G M <fergarci0103@gmail.com>
2023-12-09 16:17:10 -0600

    Documentación en Javadoc de la clase JoinView

--- src/main/java/mx/paradigmmasters/bastagui/view/JoinView.java
+++ src/main/java/mx/paradigmmasters/bastagui/view/JoinView.java
@@ -8,10 +8,26 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Objects;
 
+/**
+ * Vista para unirse al juego Basta.
+ * Esta clase proporciona una interfaz para que los usuarios ingresen su nombre de usuario y se unan al juego.
+ *
+ * @author García Martínez María Fernanda
+ * @author Hernández Meza Víctor Yahir
+ * @author Márquez López María Fernanda
+ * @author Martínez Florez Uri
+ */
 public class JoinView extends JFrame implements ActionListener {
     private MainControl control;
     private JButton bJoin;
     private JTextField tfUsername;
+
+    /**
+     * Constructor que inicializa la vista para unirse al juego.
+     *
+     * @param control Control principal del juego, utilizado para iniciar el juego una vez que el usuario se une.
+     * @throws HeadlessException Si GraphicsEnvironment.isHeadless() devuelve true.
+     */
     public JoinView(MainControl control) throws HeadlessException {
         this.control = control;
         this.setTitle("Basta");
@@ -45,6 +61,12 @@ public class JoinView extends JFrame implements ActionListener {
         this.setVisible(true);
     }
 
+    /**
+     * Maneja las acciones de los botones en la interfaz de usuario.
+     * Inicia el juego si el nombre de usuario es válido o muestra un mensaje de error en caso contrario.
+     *
+     * @param actionEvent El evento de acción generado.
+     */
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         if (this.tfUsername.getText().isEmpty()){
@@ -59,6 +81,11 @@ public class JoinView extends JFrame implements ActionListener {
         }
     }
 
+    /**
+     * Muestra un mensaje de error si hay un problema de conexión.
+     *
+     * @param attempts El número de intentos restantes para conectarse.
+     */
     public void connectionError(int attempts){
         JOptionPane.showMessageDialog(
                 this,
